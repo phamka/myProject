ac2ed7631889920e8fd62b26bc503d4f4efb67b1
phamka <feiwon185@gmail.com>
2023-03-20 12:42:13 +0000

    First committed

---
+++ jascriptStart/index.js
@@ -0,0 +1,14 @@
+// comment
+console.log('Hello There');
+let name = 'Ka';
+console.log(name);
+
+let interestRate = 0.3;
+console.log(interestRate);
+
+function sayHello () {
+    console.log('Hello Yo');
+    
+}
+
+sayHello();
---
+++ javaStart/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello there");
+
+        int num = 10, num1 = 20;
+
+        System.out.println(num + num1);
+        System.out.println();
+        
+    }
+}
